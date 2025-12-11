85bdac14740070d61cc7128a23ed586361a27a1c
Shohrux <shohruxnizomov170@gmail.com>
2025-12-11 22:57:51 +0500

    feat(project): project modeli qoshildi va fillable , casts maydonlari toldirildi

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,9 +2,42 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class Project extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'title',
+        'slug',
+        'short_description',
+        'full_description',
+        'tech_stack',
+        'github_link',
+        'live_link',
+        'zip_file_path',
+        'cover_image_path',
+        'is_featured',
+        'order',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     */
+    protected $casts = [
+        'tech_stack' => 'array',
+        'is_featured' => 'boolean',
+        'order' => 'integer',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+
 }
